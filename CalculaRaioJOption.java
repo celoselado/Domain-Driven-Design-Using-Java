0814aa7c1d2dc8928233b985a7e85eabacc8734b
Xande <112678725+celoselado@users.noreply.github.com>
2025-03-24 11:50:32 -0300

    Add files via upload

---
+++ CalculaRaioJOption.java
@@ -0,0 +1,26 @@
+package br.com.fiap.JOptionPane;
+
+import javax.swing.*;
+
+public class CalculaRaioJOption {
+    public static void main(String[] args) {
+
+        final float pi = 3.14f;
+        float raio, area;
+        String auxiliar;
+
+        try {
+
+            auxiliar = JOptionPane.showInputDialog("Digite o raio do circulo: ");
+            raio = Float.parseFloat(auxiliar);
+
+            area = (raio * raio) * pi;
+
+            JOptionPane.showMessageDialog(null, "A área do circulo é de: "+area);
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"Ocorreu um erro! Reinicie o programa e reinsira as informações corretamente.");
+        }
+
+    }
+}
